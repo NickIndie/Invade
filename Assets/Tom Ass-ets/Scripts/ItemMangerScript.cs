cca18ca81987c4806f66eff667786a1f20e5b35f
Vulpen <musicaltommy@gmail.com>
2016-05-18 23:20:56 -0400

    Item Management
    Created an individual item manager to keep track of what the player
    might pick up.

---
+++ Assets/Tom Ass-ets/Scripts/ItemMangerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMangerScript : MonoBehaviour {
+	public int TotWeight;
+	public int TotItems;
+	public int TotPrice;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Tom Ass-ets/Scripts/Player_ItemPickup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_ItemPickup : MonoBehaviour {
+	public GameObject manager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
